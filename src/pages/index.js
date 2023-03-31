a4d2739172afce1b406187ff4fa743266c3c384c
Kristijan Jovanovski <kristijan.j92@gmail.com>
2023-03-31 14:27:14 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import favicon from '../images/icon.png';
 
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 
 // Step 2: Define your component
@@ -9,6 +10,10 @@ const IndexPage = () => {
     return (
         <Layout pageTitle="Welcome to my Gatsby site!">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage
+                alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+                src="../images/clifford.webp"
+            />
         </Layout>
     );
 };
